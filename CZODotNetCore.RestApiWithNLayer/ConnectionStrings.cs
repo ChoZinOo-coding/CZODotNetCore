92c53ae2ca907e5645c9743da9139b30c3291e59
Cho Zin Oo <chozinoo@gmail.com>
2024-05-12 11:48:15 +0630

    .

---
+++ CZODotNetCore.RestApiWithNLayer/ConnectionStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CZODotNetCore.RestApiWithNLayer
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "cho123!",
+            TrustServerCertificate = true
+
+        };
+    }
+}
---
+++ CZODotNetCore.RestApiWithNLayer/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using CZODotNetCore.RestApiWithNLayer.Models;
+global using Microsoft.EntityFrameworkCore;
