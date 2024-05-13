e1db1bfeb6923749a08ed75f9f25ff9c8d5e931a
Cho Zin Oo <chozinoo@gmail.com>
2024-05-13 11:27:14 +0630

    .

---
+++ CZODotNetCore.ConsoleAppHttpClientDream/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonStr = await File.ReadAllTextAsync("DreamDictionary.json");
+var model = JsonConvert.DeserializeObject<MainDto2>(jsonStr);
+
+//Console.WriteLine(jsonStr);
+
+foreach (var question in model.BlogHeader)
+{
+    Console.WriteLine(question.BlogId);
+}
+// JSON to C# ??? package 
+// C# to JSON
+
+Console.ReadLine();
+
+
+public class MainDto2
+{
+    public Blogheader[] BlogHeader { get; set; }
+    public Blogdetail[] BlogDetail { get; set; }
+}
+
+public class Blogheader
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+}
+
+public class Blogdetail
+{
+    public int BlogDetailId { get; set; }
+    public int BlogId { get; set; }
+    public string BlogContent { get; set; }
+}
---
+++ CZODotNetCore.RestApiWithNLayer/Features/DreamDictionary/DreamDictionaryController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace CZODotNetCore.RestApiWithNLayer.Features.DreamDictionary
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DreamDictionaryController : ControllerBase
+    {
+        private DreamDictionary _data;
+
+        private async Task<DreamDictionary> GetDataAsync()
+        {
+
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<DreamDictionary>(jsonStr);
+            return model;
+
+        }
+
+        // api/LatHtaukBayDin/questions
+        [HttpGet("questions")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.BlogHeader);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.BlogDetail);
+        }
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answser(int questionNo, int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.BlogDetail.FirstOrDefault(x => x.BlogId == questionNo && x.BlogDetailId == no));
+        }
+    }
+
+        public class DreamDictionary
+    {
+        public Blogheader[] BlogHeader { get; set; }
+        public Blogdetail[] BlogDetail { get; set; }
+    }
+
+    public class Blogheader
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+    }
+
+    public class Blogdetail
+    {
+        public int BlogDetailId { get; set; }
+        public int BlogId { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+}
