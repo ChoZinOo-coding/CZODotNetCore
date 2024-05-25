bc5d354d7a7e71aad9fd3e907d6dfe7d4e36cab0
Cho Zin Oo <chozinoo@gmail.com>
2024-05-25 13:22:49 +0630

    .

---
+++ CZODotNetCore.WinFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CZODotNetCore.WinFormsApp
+{
+    partial class FrmBlog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTitle = new TextBox();
+            txtAuthor = new TextBox();
+            txtContent = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnCancel = new Button();
+            btnSave = new Button();
+            SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(103, 64);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(505, 31);
+            txtTitle.TabIndex = 1;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(103, 126);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.Size = new Size(505, 31);
+            txtAuthor.TabIndex = 2;
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(103, 188);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(505, 57);
+            txtContent.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(103, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 25);
+            label1.TabIndex = 4;
+            label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(103, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Author :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(103, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 25);
+            label3.TabIndex = 6;
+            label3.Text = "Content :";
+            // 
+            // btnCancel
+            // 
+            btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.Location = new Point(104, 259);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(112, 34);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(100, 181, 246);
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.Location = new Point(231, 259);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(112, 34);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "&Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSave);
+            Controls.Add(btnCancel);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtContent);
+            Controls.Add(txtAuthor);
+            Controls.Add(txtTitle);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+
+        private TextBox txtTitle;
+        private TextBox txtAuthor;
+        private TextBox txtContent;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnCancel;
+        private Button btnSave;
+    }
+}
