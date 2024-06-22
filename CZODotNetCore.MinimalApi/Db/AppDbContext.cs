b9f577ef5dcec583da8506dab256eda507933a36
Cho Zin Oo <chozinoo@gmail.com>
2024-06-22 13:55:56 +0630

    Complete Minimal Api CRUD

---
+++ CZODotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CZODotNetCore.MinimalApi.Db;
+using CZODotNetCore.MinimalApi.Models;
+
+namespace CZODotNetCore.MinimalApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
